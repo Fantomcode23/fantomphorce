c7c2a43bbe068c4a063f5f16640cefbfca8f33c1
kart2004 <karthikprakash999@gmail.com>
2024-05-16 21:44:13 +0530

    3rd commit

--- flask-server/models.py
+++ flask-server/models.py
@@ -1,14 +1,26 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from . import db
+from database import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    emissions = db.relationship('Emission', backref='author', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+class Emission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    car_emissions = db.Column(db.Float)
+    two_wheeler_emissions = db.Column(db.Float)
+    air_travel_emissions = db.Column(db.Float)
+    electricity_emissions = db.Column(db.Float)
+    diet_emissions = db.Column(db.Float)
+    waste_emissions = db.Column(db.Float)
+    total_emissions = db.Column(db.Float)
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
-from flask_login import login_user
-#from models import User
+from flask_login import login_user, current_user, LoginManager
+from models import User, Emission
+from database import db
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+app.config['SECRET_KEY'] = 'kar-anshul-gaya-aish'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
@@ -16,12 +31,18 @@ def login():
         if user is None or not user.check_password(request.form['password']):
             return 'Invalid username or password'
         login_user(user)
-        return 'Logged in successfully'
+        return render_template('calculator.html')
     return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return "Signup Page"
+    if request.method == 'POST':
+        user = User(username=request.form['username'])
+        user.set_password(request.form['new_password'])
+        db.session.add(user)
+        db.session.commit()
+        return render_template('login.html')
+    return render_template('signup.html')
 
 @app.route('/calc')
 def calc():
@@ -46,29 +67,51 @@ FACTORS = {
     }
 }
 
-@app.route('/calculate', methods=['POST'])
+@app.route('/calculate', methods=['GET','POST'])
 def calculate_emissions():
-    data = request.get_json()
-    country = data['country']
-    car_distance = float(data['car_distance']) * 365
-    two_wheeler_distance = float(data['two_wheeler_distance']) * 365
-    air_travel_distance = float(data['air_travel_distance']) 
-    electricity = float(data['electricity']) * 12
-    meals = float(data['meals']) * 365
-    waste = float(data['waste']) * 52
+    if request.method == 'POST':
+        data = request.get_json()
+        country = data['country']
+        car_distance = float(data['car_distance']) * 365
+        two_wheeler_distance = float(data['two_wheeler_distance']) * 365
+        air_travel_distance = float(data['air_travel_distance']) 
+        electricity = float(data['electricity']) * 12
+        meals = float(data['meals']) * 365
+        waste = float(data['waste']) * 52
 
-    car_emissions = FACTORS[country]["Car"] * car_distance
-    two_wheeler_emissions = FACTORS[country]["TwoWheeler"] * two_wheeler_distance
-    air_travel_emissions = FACTORS[country]["AirTravel"] * air_travel_distance
-    electricity_emissions = FACTORS[country]["Electricity"] * electricity
-    diet_emissions = FACTORS[country]["Diet"] * meals
-    waste_emissions = FACTORS[country]["Waste"] * waste
+        car_emissions = FACTORS[country]["Car"] * car_distance
+        two_wheeler_emissions = FACTORS[country]["TwoWheeler"] * two_wheeler_distance
+        air_travel_emissions = FACTORS[country]["AirTravel"] * air_travel_distance
+        electricity_emissions = FACTORS[country]["Electricity"] * electricity
+        diet_emissions = FACTORS[country]["Diet"] * meals
+        waste_emissions = FACTORS[country]["Waste"] * waste
 
-    total_emissions = round(
-        car_emissions + two_wheeler_emissions + air_travel_emissions + electricity_emissions + diet_emissions + waste_emissions, 2
-    )
-
-    return jsonify({
+        total_emissions = round(
+            car_emissions + two_wheeler_emissions + air_travel_emissions + electricity_emissions + diet_emissions + waste_emissions, 2
+        )
+        emission = Emission.query.filter_by(user_id=current_user.id).first()
+        if emission:
+            emission.car_emissions = car_emissions
+            emission.two_wheeler_emissions = two_wheeler_emissions
+            emission.air_travel_emissions = air_travel_emissions
+            emission.electricity_emissions = electricity_emissions
+            emission.diet_emissions = diet_emissions
+            emission.waste_emissions = waste_emissions
+            emission.total_emissions = total_emissions
+        else:
+            emission = Emission(
+                user_id=current_user.id,
+                car_emissions=car_emissions,
+                two_wheeler_emissions=two_wheeler_emissions,
+                air_travel_emissions=air_travel_emissions,
+                electricity_emissions=electricity_emissions,
+                diet_emissions=diet_emissions,
+                waste_emissions=waste_emissions,
+                total_emissions=total_emissions
+            )
+            db.session.add(emission)
+        db.session.commit()
+        return jsonify({
         'car_emissions': round(car_emissions / 1000, 2),
         'two_wheeler_emissions': round(two_wheeler_emissions / 1000, 2),
         'air_travel_emissions': round(air_travel_emissions / 1000, 2),
@@ -77,6 +120,14 @@ def calculate_emissions():
         'waste_emissions': round(waste_emissions / 1000, 2),
         'total_emissions': round(total_emissions / 1000, 2)
     })
+    else:
+        emission = Emission.query.filter_by(user_id=current_user.id).first()
+        if emission:
+            print(emission)
+            return render_template('calculate.html', emission=emission)
+        else:
+            return render_template('calculate.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
